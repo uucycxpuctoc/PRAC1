2c34f99fa528b4c935689ce6dd7dd0a65c878f42
Дышленко Александр <devilxhant@bk.ru>
2026-02-03 19:20:01 +0500

    Delete src/js/navigation.js

--- src/js/navigation.js
+++
@@ -1,8 +0,0 @@
-const pages = document.querySelectorAll('.page');
-
-document.querySelectorAll('[data-target]').forEach(btn => {
-  btn.addEventListener('click', () => {
-    pages.forEach(p => p.classList.remove('active'));
-    document.getElementById(btn.dataset.target).classList.add('active');
-  });
-});
